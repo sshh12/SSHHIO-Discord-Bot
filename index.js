a05234e20f6140b8458b90047a80d3ff488f5593
Shrivu Shankar <shrivu1122@gmail.com>
2017-12-30 17:08:46 -0600

    AutoReacts

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const creds = require('./creds.json');
+const settings = require('./settings.json');
 
-const creds = require('./creds.json')
+const autoreact = require('./plugins/autoreact/autoreact');
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('Connected!');
+});
+
+client.on('message', message => {
+  if (settings.channels.includes(message.channel.name)) {
+    autoreact(client, message);
+  }
 });
 
 client.login(creds.token);
---
+++ plugins/autoreact/autoreact.js
@@ -0,0 +1,22 @@
+const reacts = require('./autoreact.json');
+
+module.exports = function autoreact(client, message) {
+
+  if (reacts.enabled) {
+
+    let msg = message.content
+                     .toLowerCase()
+                     .replace(/[^\w\s]|_/g, "")
+                     .trim().split(" ");
+
+    for (let word of msg) {
+      for (let emoji in reacts.reacts) {
+        if (reacts.reacts[emoji].includes(word)) {
+          message.react(client.emojis.find("name", emoji));
+        }
+      }
+    }
+
+  }
+
+}
